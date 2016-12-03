a7e982212ae5ace1e24c9b97f39b70a2ae59bc1c
Mario Lopez <mario@techmunchies.net>
2016-12-02 22:58:38 -0500

    Added function for section visibility, made function for nav scroll anonymous, and adding events section

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
 // Check position of scroll, and if it's greater than 80px, add the scrolled
 // class to the nav
-function nav() {
-	var yPos = $(window).scrollTop();
+$(function() {
+  return $(window).on('scroll', function() {
+    var yPos = $(window).scrollTop();
 
-	if (yPos >= 80) {
-		$('.nav').addClass('scrolled');
-	}
-	else {
-		$('.nav').removeClass('scrolled');
-	}
-}
+  	if (yPos >= 80) {
+  		$('.nav').addClass('scrolled');
+  	}
+  	else {
+  		$('.nav').removeClass('scrolled');
+  	}
+  });
+});
 
-nav();
+// Checks if an element is in the viewport
+var isElementInViewport = function($e, yPos) {
 
-$(window).scroll(function() {
-	nav();
+	// Hold the top and bottom for the element
+  var yTop, yBottom;
+
+  yTop = $e.offset().top - yPos;
+	yBottom = $e.offset().top + $e.outerHeight();
+
+  if (yTop <= $(window).height() && yBottom - yPos >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Checks the element position in relation to the viewport
+$(function() {
+
+	// Hidden sections
+	var $sections;
+
+	$sections = $('.hidden-section');
+
+  return $(window).on('scroll', function() {
+    return $sections.each(function() {
+      var elementInViewport = isElementInViewport($(this), $(window).scrollTop() - 150);
+      if (elementInViewport && !$(this).hasClass('visible-section')) {
+        return $(this).addClass('visible-section');
+      }
+    });
+  });
 });
