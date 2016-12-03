f19c7a27db176af0b28a9ab68eae5692dbbbfa42
Mario Lopez <mario@techmunchies.net>
2016-12-02 22:06:30 -0500

    Removed lines code, will not be needed

--- main.js
+++ main.js
@@ -16,21 +16,3 @@ nav();
 $(window).scroll(function() {
 	nav();
 });
-
-// Create lines and glitch effects modified from: http://jsfiddle.net/M34JL/2/
-
-// Loads title
-title = $(".title").html();
-
-// Creates lines throughout the site
-function createlines() {
-  var n = $(document).height() - ($(document).height() / 100 * 5); // Gets the height of the page
-  console.log(n);
-  for(var i = 0; i < n / 2; i++){
-    $(".container").append("<div class='line'></div>"); // Appends lines
-  }
-}
-
-createlines(); // Create lines
-
-window.addEventListener('load', createlines()); // Add to load of site page
